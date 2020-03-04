b071d53fc5c03fcf6819d3f3802c283497a3eee2
Ky Kyler <ky.kyler1@gmail.com>
2020-03-04 13:29:14 -0500

    adjusted timer code, added JQ

--- DevOpsFiles/Java.js
+++ DevOpsFiles/Java.js
@@ -1,1 +1,33 @@
-// 
+var secLeft = 1800;
+var countDown = setInterval(decreaseTime, 1000);
+
+$( window ).unload(function() {
+    return "Handler for .unload() called.";
+  });
+
+// decrease Timer
+function decreaseTime() {
+  var secLeft = secLeft -1;
+  if (secLeft = 0 ){
+    clearInterval(secLeft);
+  }
+
+  }
+
+
+// Timer Div
+
+
+{/* //  Event listerner-check if correct button is true, change page add or subtract points.
+  // generateBtn.addEventListener("click",function(event)){ */}
+  
+  // if (numBox.checked === false && symbox.checked === false && upperBox.checked === false &&   charbox.checked === false){
+  //         alert("Please select your criteria");
+  //     }
+  //     else {
+  //     var pwLength = prompt("input a number length here");
+  
+  //     var possiblePasswordCharacters = '';
+  //     if (numBox.checked === true ){
+  //     possiblePasswordCharacters = possiblePasswordCharacters + num; 
+  //     } 
