e1b405b78f8213ee77a71ee2e46e308011e2db62
Ky Kyler <ky.kyler1@gmail.com>
2020-03-04 22:36:08 -0500

    updated code

--- DevOpsFiles/Java.js
+++ DevOpsFiles/Java.js
@@ -1,22 +1,26 @@
 var secLeft = 1800;
 var countDown = setInterval(decreaseTime, 1000);
 
-$( window ).unload(function() {
-    return "Handler for .unload() called.";
+$( document).load(function() {
+  function decreaseTime() {
+     secLeft = secLeft -1;
+    if (secLeft < 1 ){
+      clearInterval(secLeft);
+    }
   });
 
 // decrease Timer
-function decreaseTime() {
-  var secLeft = secLeft -1;
-  if (secLeft = 0 ){
-    clearInterval(secLeft);
-  }
+
 
   }
-
+document.getElementsByClassName()
 
 // Timer Div
 
+// how to chcek the class to apply the correct function to it
+// how to get class name of an element- get the class name
+// for each input field see if they class name is correct or not. 
+// if the class  name is corerct do this else do this. 
 
 {/* //  Event listerner-check if correct button is true, change page add or subtract points.
   // generateBtn.addEventListener("click",function(event)){ */}
